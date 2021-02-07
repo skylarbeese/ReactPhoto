0987b42052ede512de5d45ab6361ac24e0888797
SKYLAR BEESE <skylarbeese@gmail.com>
2021-02-06 17:23:46 -0700

    commit2

--- react-app/src/comp/heroImg.jsx
+++ react-app/src/comp/heroImg.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Arr from './../imgArray'
 
 function HeroImg({slides}) {
     const[active, setActive] = React.useState(0)
+
+
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        setActive(active === length  - 1 ? 0 : active + 1)
+      }, 4000)
+      return () => clearInterval(interval)
+    }, [active])
+
+    
     const length = slides.length
     
     const nextSlide = () => {
