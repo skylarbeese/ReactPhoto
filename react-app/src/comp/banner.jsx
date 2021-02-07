4d0e14c33b06bfdbc252284cb129afc6effb217f
SKYLAR BEESE <skylarbeese@gmail.com>
2021-02-06 17:49:18 -0700

    c

--- react-app/src/comp/banner.jsx
+++ react-app/src/comp/banner.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Arr from './../imgArray'
-
-function Banner({banner}) {
+import {useWindowScroll} from 'react-use'
+function Banner() {
+    const[show, setShow] = React.useState(false)
+    const {y: pageYOffset } = useWindowScroll()
+    React.useEffect(() => {
+       if(pageYOffset > 600) {
+          setShow(true)
+          
+       }
+    }, [pageYOffset])
   return (
    <>
     <div className="banner-con">
-        <div className="banner-text">
+       {show ? (<div className="banner-text">
             <h1>This is the Title</h1>
             <div className="underline"></div>
             <p>Bacon ipsum dolor amet salami venison doner flank. Bacon prosciutto pork belly 
                 capicola tri-tip burgdoggen. Chislic meatloaf meatball, beef ribs 
                
             </p>
-        </div>
+        </div>) : ''}
         <div className="grad"></div>
         <div className="banner-img">
        
